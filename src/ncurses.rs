b67a9eb0f9383aea48e7c7be7528b4770532b680
Thomas Terry <thomas.terry@coramdeoacademy.org>
2021-07-01 22:32:27 -0500

    Added subwindow and optimized trait impl

--- src/ncurses.rs
+++ src/ncurses.rs
@@ -1,9 +1,18 @@
 use ncurses::*;
 
 pub trait Window {
-    fn mv_print(&mut self, x: i32, y: i32, text: &str);
-    fn print(&mut self, text: &str);
-    fn read_char(&self) -> char;
+    fn mv_print(&mut self, x: i32, y: i32, text: &str) {
+        mvaddstr(y, x, text);
+    }
+
+    fn print(&mut self, text: &str) {
+        addstr(text);
+    }
+
+    fn read_char(&self) -> char {
+        getch() as u8 as char
+    }
+
 }
 
 pub trait MovableWindow {
@@ -11,29 +20,15 @@ pub trait MovableWindow {
 }
 
 pub struct MainWindow {
-    this: WINDOW,
 }
 
 impl Window for MainWindow {
-    fn mv_print(&mut self, x: i32, y: i32, text: &str) {
-        mvwaddstr(self.this, y, x, text);
-    }
-
-    fn print(&mut self, text: &str) {
-        waddstr(self.this, text);
-    }
-
-    fn read_char(&self) -> char {
-        getch() as u8 as char
-    }
 }
 
 impl MainWindow {
     pub fn init() -> Self {
         initscr();
-        MainWindow {
-            this: stdscr(),
-        }
+        MainWindow { }
     }
 }
 
@@ -42,5 +37,33 @@ impl Drop for MainWindow {
     // and end ncurses
     fn drop(&mut self) {
         endwin();
+    }
+}
+
+pub struct SubWindow {
+    this: WINDOW,
+}
+
+impl SubWindow {
+    pub fn init(x: i32, y: i32, start_x: i32, start_y: i32) -> Self {
+        let win = newwin(y,x, start_y, start_x);
+
+        SubWindow { this: win }
+    }
+}
+
+impl Window for SubWindow {
+    fn mv_print(&mut self, x: i32, y: i32, text: &str) {
+        mvwaddstr(self.this, y, x, text);
+    }
+
+    fn print(&mut self, text: &str) {
+        waddstr(self.this, text);
+    }
+}
+
+impl Drop for SubWindow {
+    fn drop(&mut self) {
+        delwin(self.this);
     }
 }
