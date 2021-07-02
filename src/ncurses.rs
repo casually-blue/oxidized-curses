83a2c38121311085bf9e77bd7aa75bd474497217
Thomas Terry <thomas.terry@coramdeoacademy.org>
2021-07-01 19:26:17 -0500

    Added window trait

--- src/ncurses.rs
+++ src/ncurses.rs
@@ -1,17 +1,30 @@
 use ncurses::*;
 
-pub struct MainWindow {
+pub trait Window {
+    fn create(height: u32, width: u32, startx: u32, starty: u32);
+    fn move_win(&mut self, x: u32, y: u32);
+}
 
+pub struct MainWindow {
+    this: WINDOW,
 }
 
 impl MainWindow {
     pub fn init() -> Self {
         initscr();
-        MainWindow {}
+        MainWindow {
+            this: stdscr(),
+        }
     }
 
-    pub fn print(&self, x: i32, y: i32, text: &str) {
-       mvaddstr(y, x, text);
+    // print a string to the screen at a specific location
+    // note: position parameters are backwards from ncurses
+    pub fn mvprint(&mut self, x: i32, y: i32, text: &str) {
+       mvwaddstr(self.this, y, x, text);
+    }
+
+    pub fn print(&mut self, text: &str) {
+        waddstr(self.this, text);
     }
 
     pub fn read_char(&self) -> char {
@@ -20,6 +33,8 @@ impl MainWindow {
 }
 
 impl Drop for MainWindow {
+    // reset terminal to initial state
+    // and end ncurses
     fn drop(&mut self) {
         endwin();
     }
