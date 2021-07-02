1b8ffaf1cdd017258877302e13477ddd0366ddc6
Thomas Terry <thomas.terry@coramdeoacademy.org>
2021-07-02 15:36:09 -0500

    Add docs to functions

--- src/ncurses.rs
+++ src/ncurses.rs
@@ -1,5 +1,6 @@
 use ncurses::*;
 
+// default printing functions for ncurses
 pub trait Window {
     fn mv_print(&mut self, x: u32, y: u32, text: &str) {
         mvaddstr(y as i32, x as i32, text);
@@ -29,6 +30,8 @@ impl Window for MainWindow {
 }
 
 impl MainWindow {
+    // initialize a reasonable starting
+    // state for the main window
     pub fn init() -> Self {
         initscr();
         set_cbreak(true);
@@ -42,6 +45,8 @@ impl MainWindow {
     }
 }
 
+// tell ncurses whether to echo typed
+// characters to the screen
 fn set_echo(echo_val: bool){
     if echo_val {
         echo();
@@ -50,6 +55,7 @@ fn set_echo(echo_val: bool){
     }
 }
 
+// set whether ncurses gets raw input or not
 fn set_cbreak(cbreak_val: bool){
     if cbreak_val {
         cbreak();
@@ -76,6 +82,7 @@ pub struct SubWindow {
 }
 
 impl SubWindow {
+    // create the new subwindow and get its information
     pub fn init(x: u32, y: u32, start_x: u32, start_y: u32) -> Self {
         let win = newwin(y as i32,x as i32, start_y as i32, start_x as i32);
 
@@ -84,16 +91,19 @@ impl SubWindow {
 }
 
 impl Window for SubWindow {
+    // move the cursor and then print a string to the screen
     fn mv_print(&mut self, x: u32, y: u32, text: &str) {
         mvwaddstr(self.this, y as i32, x as i32, text);
     }
 
+    // print a string to the screen at the current cursor position
     fn print(&mut self, text: &str) {
         waddstr(self.this, text);
     }
 }
 
 impl Drop for SubWindow {
+    // delete the ncurses window instance
     fn drop(&mut self) {
         delwin(self.this);
     }
