cbe0688505f5be03a3387a7fe4ab011ba96a5c29
Thomas Terry <thomas.terry@coramdeoacademy.org>
2021-07-05 19:21:15 -0500

    namespace everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-mod cursor;
-mod io_attrs;
-mod utils;
-mod window;
-
-pub use cursor::*;
-pub use io_attrs::*;
-pub use utils::*;
-pub use window::*;
+pub mod cursor;
+pub mod io_attrs;
+pub mod utils;
+pub mod window;
